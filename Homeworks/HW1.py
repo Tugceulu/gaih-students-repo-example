3decfd4961d6fd440127f6ef4edeb3ad23350f19
Tugceulu <67679907+Tugceulu@users.noreply.github.com>
2021-03-09 21:31:01 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,4 +1,10 @@
 #Explain your work
 
 #Question 1
-for x in range(a):
+list1=[1,3,5,7,9]
+list2=[0,2,4,6,8]
+list=list1+list2
+list=list*2
+print("New list:",list)
+for i in list:
+    print("Type of ",i," is: ",type(i),sep="")
